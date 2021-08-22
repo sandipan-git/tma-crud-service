3ec82deb445339df694cb83d4187e0ac016fec72
Sandipan Sarkar <work.sandipan@gmail.com>
2021-08-22 13:09:43 +0530

    Initial Commit: Project Structure

---
+++ src/main/java/com/sandipan/work/tma/TMACRUDServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sandipan.work.tma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TMACRUDServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TMACRUDServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandipan/work/tma/TMACRUDServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandipan.work.tma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TMACRUDServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
